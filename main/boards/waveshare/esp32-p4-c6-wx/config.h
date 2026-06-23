1e3d44f57690c43c74554868ffc3573909214153
pwdzsj-work <pwdzsj@gmail.com>
2026-06-23 17:54:27 +0800

    配置esp32-p4-c6项目audio引脚

--- main/boards/waveshare/esp32-p4-c6-wx/config.h
+++ main/boards/waveshare/esp32-p4-c6-wx/config.h
@@ -10,6 +10,10 @@
 #define AUDIO_INPUT_SAMPLE_RATE   24000
 #define AUDIO_OUTPUT_SAMPLE_RATE  24000
 
+#define ISESP32P4_WAVESHARE
+
+#ifdef ISESP32P4_WAVESHARE
+
 // ESP32-P4-WIFI6 schematic mapping:
 // ES8311: ASDOUT->GPIO11, LRCK->GPIO10, DSDIN->GPIO9, SCLK->GPIO12, MCLK->GPIO13
 #define AUDIO_I2S_GPIO_MCLK       GPIO_NUM_13
@@ -21,10 +25,30 @@
 // ES8311 I2C. xiaozhi Es8311AudioCodec uses the 8-bit address form here.
 #define AUDIO_CODEC_I2C_SDA_PIN   GPIO_NUM_7
 #define AUDIO_CODEC_I2C_SCL_PIN   GPIO_NUM_8
-#define AUDIO_CODEC_ES8311_ADDR   0x30
+#define AUDIO_CODEC_ES8311_ADDR   ES8311_CODEC_DEFAULT_ADDR
 
 // NS4150B PA_CTRL: schematic shows GPIO53 -> R68 0R -> PA_CTRL.
 #define AUDIO_CODEC_PA_PIN        GPIO_NUM_53
 #define AUDIO_CODEC_PA_INVERTED   false
+
+#else
+
+// ESP32-P4-WIFI6 schematic mapping:
+#define AUDIO_I2S_GPIO_MCLK       GPIO_NUM_53
+#define AUDIO_I2S_GPIO_BCLK       GPIO_NUM_52
+#define AUDIO_I2S_GPIO_WS         GPIO_NUM_48
+#define AUDIO_I2S_GPIO_DOUT       GPIO_NUM_54
+#define AUDIO_I2S_GPIO_DIN        GPIO_NUM_NC   //录音脚
+
+// ES8311 I2C. xiaozhi Es8311AudioCodec uses the 8-bit address form here.
+#define AUDIO_CODEC_I2C_SDA_PIN   GPIO_NUM_15
+#define AUDIO_CODEC_I2C_SCL_PIN   GPIO_NUM_14
+#define AUDIO_CODEC_ES8311_ADDR   ES8311_CODEC_DEFAULT_ADDR
+
+// NS4150B PA_CTRL: schematic shows GPIO53 -> R68 0R -> PA_CTRL.
+#define AUDIO_CODEC_PA_PIN        GPIO_NUM_43
+#define AUDIO_CODEC_PA_INVERTED   false
+#endif
+
 
 #endif  // _BOARD_CONFIG_H_
